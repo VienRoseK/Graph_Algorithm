b3f05f69eca4dfee74c31595aaf19acb05bc5784
VienRoseK <108508876+VienRoseK@users.noreply.github.com>
2022-07-01 01:01:49 -0500

    update BFS.cpp, BFS.h

--- Graph_Algorithm/BFS.cpp
+++ Graph_Algorithm/BFS.cpp
@@ -1,1 +1,40 @@
-#include "BFS.h"
+//Breadth First Search Algorithm 
+#pragma once
+#include <stdio.h>
+#include <iostream>
+
+class BFS
+{
+private:
+	bool visited = false;
+	int count = 0;
+	int stageArray[];
+
+public:
+	//BFS class constructor 
+	BFS(int numberOfInput);
+
+	//bfsOfGraph 
+
+
+
+};
+
+
+BFS::BFS(int numberOfInput) {
+
+}
+
+BFS::bfsOfGraph() {
+	count = numberOfInput;
+
+	//Create int array for stage arrangment 
+	stageArray[count];
+
+	while
+
+}
+
+
+
+
